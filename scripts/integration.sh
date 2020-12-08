df0b2604debaef31b76bd22c24df4416bbe53d4a
Micah Young <ymicah@vmware.com>
2020-12-08 09:00:32 -0500

    Refactors integration to use temporary builder
    - More realisitic usage
    
    Signed-off-by: Micah Young <ymicah@vmware.com>

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -4,17 +4,36 @@ set -o errexit -o pipefail -o nounset
 $(dirname $0)/../scripts/build.sh
 
 OS=$(docker info --format '{{.OSType}}')
-pack package-buildpack multi-os-cnb:${OS} --config package-${OS}.yml
+pack package-buildpack multi-os-cnb-${OS} --config package-${OS}.yml
 
 if [[ "$OS" == "windows" ]]; then
-BUILDER=cnbs/sample-builder:nanoserver-1809
-else 
-BUILDER=paketobuildpacks/builder:tiny
+STACK_ID=io.buildpacks.samples.stacks.nanoserver-1809
+BUILD_IMAGE=cnbs/sample-stack-build:nanoserver-1809
+RUN_IMAGE=cnbs/sample-stack-run:nanoserver-1809
+else
+STACK_ID=io.buildpacks.samples.stacks.bionic
+BUILD_IMAGE=cnbs/sample-stack-build:bionic
+RUN_IMAGE=cnbs/sample-stack-run:bionic
 fi
 
+pack create-builder multi-os-builder-${OS} --config <(cat <<EOF
+[[buildpacks]]
+uri = "docker://multi-os-cnb-${OS}"
+
+[[order]]
+[[order.group]]
+id = "multi-os"
+version = "0.0.1"
+
+[stack]
+id = "${STACK_ID}"
+run-image = "${RUN_IMAGE}"
+build-image = "${BUILD_IMAGE}"
+EOF
+)
+
 pack build multi-os-test:${OS} \
-  --buildpack docker://multi-os-cnb:${OS} \
-  --builder $BUILDER \
+  --builder multi-os-builder-${OS} \
   --path $(dirname $0)/../integration/testdata/app \
   --env "BP_GO_TARGETS=./cmd/app" \
   --trust-builder
